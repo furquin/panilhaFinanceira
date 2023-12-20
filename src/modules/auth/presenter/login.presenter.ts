2d5afc5c57333e044d73545fb21e8f860549b5ff
furquin <laert.ff@gmail.com>
2023-12-20 20:32:19 -0300

    Add ApiProperty decorators to LoginPresenter class

--- src/modules/auth/presenter/login.presenter.ts
+++ src/modules/auth/presenter/login.presenter.ts
@@ -2,8 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { UserPresenter } from 'src/modules/users/presenter/user.presenter';
 
 export class LoginPresenter {
+  @ApiProperty()
   user: UserPresenter;
-
+  @ApiProperty()
   token: string;
   constructor(user: UserPresenter, token: string) {
     this.user = user;
